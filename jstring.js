0e70924c122ec457345c6069411f14a4e8ab795f
Hani (HMK) <hmkcode@gmail.com>
2013-07-24 08:24:38 +0300

    add & modify
    1. add comments
    2. modify capitalize toLowerCase() the rest of the string
    3. add startsWith()
    4. modify endsWith() to check if(end.length > string.length)

--- jstring.js
+++ jstring.js
@@ -1,8 +1,10 @@
-﻿String.prototype.contains = function(text){
+// ( 1 ) String.contains()
+String.prototype.contains = function(text){
 		if(text == '') return true;
 		else if(text == null) return false;
 		else return this.indexOf(text) !== -1;
 }	
+// ( 2 ) String.count()
 String.prototype.count = function(text){
 		if(this.contains(text)){
 			return this.split(text).length-1;
@@ -10,19 +12,28 @@ String.prototype.count = function(text){
 		else
 			return 0;
 }
+// ( 3 ) String.capitalize()
 String.prototype.capitalize = function(){
 		if(this == '') return str;
-	    else return this.charAt(0).toUpperCase() + this.slice(1);
+	    else return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 }
+// ( 4 ) String.clear()
 String.prototype.clear = function(){
      	return this.trim().replace(/\s+/g, ' ');
 }
-String.prototype.endsWith = function(text) {
-	if(text == '') return true;
-	else if(text == null) return false;
-    else return this.indexOf(text, this.length - text.length) !== -1;
+// ( 5 ) String.startsWith()
+String.prototype.startsWith = function(start) {
+	if(start == '') return true;
+	else if(start == null || start.length > this.length) return false;
+    else return this.substring(0,start.length) == start;
 }
-
+// ( 6 ) String.endsWith()
+String.prototype.endsWith = function(end) {
+	if(end == '') return true;
+	else if(end == null || end.length > this.length) return false;
+    else return this.indexOf(end, this.length - end.length) !== -1;
+}
+// ( 7 ) String.insert()
 String.prototype.insert = function(text,at) {
 	if(at == null || at > this.length)
 		at = this.length;
